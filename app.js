c64a17251adda1263db9ccfb8b1663b0b62617f6
tecjereferreyra <tec.jereferreyra@gmail.com>
2026-05-12 09:04:53 -0300

    segundo commit app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 alert("Hola somos Maia y Jeremías");
+let tituloCurso = "Curso de Frontend";
+let descripcionCurso= "Aprendemos lenguajes como Python, Java y JavaScript."
+let precioCurso = 10000;
+let estadoCurso = "Disponible";
+
+
+document.getElementById("tituloCurso").innerHTML = tituloCurso;
+document.getElementById("descripcionCurso").innerHTML = descripcionCurso;
+document.getElementById("precioCurso").innerHTML = "El precio es: $"+precioCurso;
+document.getElementById("estadoCurso").innerHTML = estadoCurso;
